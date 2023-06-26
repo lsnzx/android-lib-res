cb47aed6527947029b3f4631e0d3118d30de44fb
lzq <761048783@qq.com>
2023-06-26 09:09:42 +0800

    修复RecyclerView 问题

--- android-lib-res/build.gradle.kts
+++ android-lib-res/build.gradle.kts
@@ -36,7 +36,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.lsn.lib"
                 artifactId = "res"
-                version = "1.0.0"
+                version = "1.0.1"
 //                from(components["java"])
                 afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
             }
